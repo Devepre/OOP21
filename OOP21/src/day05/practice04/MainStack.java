6c5dc8df77c5e3a1e5f8afe57efe2c254786384f
Devepre <bicycle2life@gmail.com>
2017-10-16 19:28:23 +0300

    added uses of regular Stack implementation

--- OOP21/src/day05/practice04/MainStack.java
+++ OOP21/src/day05/practice04/MainStack.java
@@ -1,5 +1,7 @@
 package day05.practice04;
 
+import java.util.Stack;
+
 public class MainStack {
 
 	public static void main(String[] args) {
@@ -8,13 +10,13 @@ public class MainStack {
 		myStack.push("Hello");
 		myStack.push(null);
 		System.out.println(myStack);
-		
+
 		System.out.println(myStack.pop());
 		System.out.println(myStack);
-		
+
 		System.out.println(myStack.peek());
 		System.out.println(myStack);
-		
+
 		myStack.push(0.2);
 		myStack.push("Hello");
 		myStack.push(45);
@@ -22,6 +24,16 @@ public class MainStack {
 
 		System.out.println(myStack.search("Hello"));
 		
+		//using methods inherited from Vector (which are not supposed to exist for stack)
+		Stack<Object> stack = new Stack<>();
+		stack.push(5);
+		stack.push("Hi");
+		Object obj = stack.get(1);
+		System.out.println(stack);
+		System.out.println(obj);
+		stack.remove(1);
+		System.out.println(stack);
+
 	}
 
 }
